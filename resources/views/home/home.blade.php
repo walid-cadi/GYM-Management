6154ea939d19adfeb484ff898c6461b078ce14a7
walid cadi <cadiwalid02@gmail.com>
2024-11-18 14:28:40 +0100

    hero-section

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.index')
+
+@section('content')
+    <div class="hero_section w-full min-h-screen bg-gray-800">
+        @if (Route::has('login'))
+            <nav class="flex flex-wrap items-center justify-between px-5 md:px-7 py-4">
+                <img class="w-32 md:w-48" src="{{ asset('images/FitMaster_Logo.png') }}" alt="">
+                <div class="flex items-center gap-x-3 md:gap-x-5">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-white text-sm md:text-lg font-medium">
+                            Dashboard
+                        </a>
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="bg-[#d4fdc2] rounded-full text-[#006400] text-sm md:text-lg font-semibold py-2 px-5 md:px-7"
+                        >
+                            Log in
+                        </a>
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="bg-[#006400] rounded-full text-white text-sm md:text-lg font-semibold py-2 px-5 md:px-7 hover:bg-[#75de5d] duration-300"
+                            >
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </div>
+            </nav>
+        @endif
+        <div class="mt-10 md:mt-[15vh] flex flex-col items-start gap-y-6 md:gap-y-10 px-5 sm:px-10 md:px-[10vw]">
+        <h1 class="text-white text-3xl sm:text-4xl md:text-5xl font-bold w-full md:w-[45vw] leading-tight">
+            Transform your Fitness Journey
+        </h1>
+        <a
+            href="#"
+            class="text-white bg-[#006400] text-sm md:text-lg py-3 px-5 md:px-6 rounded-lg hover:bg-[#75de5dc5] duration-300"
+        >
+            BECOME A MEMBER
+        </a>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("home.home");
+    //view('welcome');
 });
 
 Route::get('/dashboard', function () {
